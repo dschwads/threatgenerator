a7a033079fb82022f8891d14aad1eee70e4b827d
Daniel Schwarz <41798934+dschwads@users.noreply.github.com>
2023-02-14 11:57:44 -0700

    Update hostmode.sh

--- docker/hostmode.sh
+++ docker/hostmode.sh
@@ -4,7 +4,7 @@
   # Download the Microsoft repository GPG keys
   wget -q https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb && \
   # Register the Microsoft repository GPG keys
-  dpkg -i packages-microsoft-prod.deb && \
+  sudo dpkg -i packages-microsoft-prod.deb && \
   # Update the list of packages after we added packages.microsoft.com
   sudo apt update  && 
     sudo apt install -y apt-transport-https ca-certificates gnupg software-properties-common at ccrypt clang cron curl ed g++ gcc golang-go iproute2 iputils-ping kmod libpam0g-dev less lsof make netcat net-tools nmap p7zip python2 rsync samba selinux-utils ssh sshpass sudo tcpdump telnet tor ufw vim wget whois zip 
